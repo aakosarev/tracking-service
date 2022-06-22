b9f67f97db90d0d1c98ca005710f5421c8ad3be0
aakosarev <kosarevjob@gmail.com>
2022-06-22 20:29:01 +0300

    some changes

--- internal/tracking/handler.go
+++ internal/tracking/handler.go
@@ -20,61 +20,95 @@ func (h *Handler) Register(router *httprouter.Router) {
 }
 
 func (h *Handler) Track(w http.ResponseWriter, r *http.Request) {
+	h.Logger.Info("Put tracking data in database")
+	w.Header().Set("Content-Type", "application/json")
 	trackingNumber := r.URL.Query().Get("tracking_number")
 	courierCode := r.URL.Query().Get("courier_code")
 	if trackingNumber == "" || courierCode == "" {
-		http.Error(w, "Request type error", 400)
+		w.WriteHeader(400)
+		w.Write([]byte(`{"message":"Request type error
+						            Please check the API documentation for the request type of this API"}`))
 		return
 	}
 	inputData := InputData{TrackingNumber: trackingNumber, CourierCode: courierCode}
 	buf, err := json.Marshal(inputData)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Server error: %s", err.Error()), 511)
+		w.WriteHeader(500)
+		w.Write([]byte(`{"message":"Server error
+									Please contact us: kosarevjob@gmail.com"}`))
 		return
 	}
 	cfg := config.GetConfig()
 	reqURL := fmt.Sprintf("%s%s", cfg.TrackingMore.BaseUrl, "/v3/trackings/create")
-	req, err := http.NewRequest(http.MethodPost, reqURL, bytes.NewBuffer(buf))
+	req1, err := http.NewRequest(http.MethodPost, reqURL, bytes.NewBuffer(buf))
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Server error: %s", err.Error()), 511)
+		w.WriteHeader(500)
+		w.Write([]byte(`{"message":"Server error
+									Please contact us: kosarevjob@gmail.com"}`))
 		return
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Tracking-Api-Key", cfg.TrackingMore.ApiKey)
-	defer req.Body.Close()
+	req1.Header.Set("Content-Type", "application/json")
+	req1.Header.Set("Tracking-Api-Key", cfg.TrackingMore.ApiKey)
+	defer req1.Body.Close()
 
 	httpClient := &http.Client{}
 
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		fmt.Println(err)
+	resp1, _ := httpClient.Do(req1)
+	defer resp1.Body.Close()
+
+	m := map[string]interface{}{}
+	if err := json.NewDecoder(resp1.Body).Decode(&m); err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(`{"message":"Server error
+									Please contact us: kosarevjob@gmail.com"}`))
+		return
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK || resp.StatusCode == 423 {
+
+	status := int(m["code"].(float64))
+	h.Logger.Debug(status)
+
+	if status == http.StatusOK || status == 423 {
 		reqURL = fmt.Sprintf("%s%s", cfg.TrackingMore.BaseUrl, fmt.Sprintf("/v3/trackings/get?tracking_numbers=%s", trackingNumber))
-		req, err = http.NewRequest(http.MethodGet, reqURL, nil)
+		req2, err := http.NewRequest(http.MethodGet, reqURL, nil)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Server error: %s", err.Error()), 511)
+			w.WriteHeader(500)
+			w.Write([]byte(`{"message":"Server error
+									    Please contact us: kosarevjob@gmail.com"}`))
 			return
 		}
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Tracking-Api-Key", cfg.TrackingMore.ApiKey)
-		resp, err = httpClient.Do(req)
-		if err != nil {
-			fmt.Println(err)
-		}
-		trackingResult := TrackingResult{}
-		if err = json.NewDecoder(resp.Body).Decode(&trackingResult); err != nil {
-			fmt.Println(err)
-		}
-		body, err := json.Marshal(trackingResult)
-		if err != nil {
-			fmt.Println(err)
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(body)
+		req2.Header.Set("Content-Type", "application/json")
+		req2.Header.Set("Tracking-Api-Key", cfg.TrackingMore.ApiKey)
+		defer req1.Body.Close()
 
+		resp2, _ := httpClient.Do(req2)
+		defer resp2.Body.Close()
+
+		trackingResult := TrackingResult{}
+		if err = json.NewDecoder(resp2.Body).Decode(&trackingResult); err != nil {
+			http.Error(w, fmt.Sprintf("Server error 5: %s", err.Error()), 500)
+			w.Write([]byte(`{"message":"Server error
+									    Please contact us: kosarevjob@gmail.com"}`))
+			return
+		}
+
+		status := int(trackingResult.Code)
+		h.Logger.Debug(status)
+
+		if status != http.StatusOK {
+			w.WriteHeader(status)
+			w.Write([]byte(fmt.Sprintf(`{"message":"Message from TrackingMore: %s"}`, trackingResult.Message)))
+			return
+		}
+
+		body, _ := json.Marshal(trackingResult) // TODO: Delete this
+		h.Logger.Debug(string(body))            // TODO: Delete this
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"Request response is successful"}`))
+		return
 	} else {
-		w.WriteHeader(resp.StatusCode)
+		w.WriteHeader(status)
+		w.Write([]byte(fmt.Sprintf(`{"message":"Message from TrackingMore: %s"}`, m["message"])))
+		return
 	}
 }
